8d2c98982b9882d4226edb6f992b8a532cc8e00e
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2022-06-30 17:35:33 +0200

    Fix import bug

---
+++ utils/model.py
@@ -0,0 +1,20 @@
+import openai
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def gpt3(prompt):
+    """ functions to call GPT3 predictions """
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=20,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        logprobs=1
+    )
+    return response.choices[0]['text'], response.choices[0]['logprobs']['tokens'], response.choices[0]['logprobs'][
+        'token_logprobs']
