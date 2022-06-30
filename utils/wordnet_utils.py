07d1cf2958fe8da5a223063054d29cdae1dc3c23
Dimitris Alivanistos <dimitris.alivas@gmail.com>
2022-06-30 13:36:22 +0200

    updates

--- utils/wordnet_utils.py
+++ utils/wordnet_utils.py
@@ -2,25 +2,51 @@ from nltk.corpus import wordnet as wn
 import nltk
 import random
 
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
 
 
 def distort_sentence(sentence: str) -> str:
+    """
+        Distorts the input sentence with by replacing random tokens with synonyms.
+    :param sentence:
+    :return:
+    """
     words = sentence.split(" ")
     for i in range(5):
-      replace_index = random.randint(0, len(words) -2)
-      word = words[replace_index]
-      synonyms = wn.synsets(word)
-      if len(synonyms) > 0:
-        synonym = synonyms[0].lemmas()[0].name()
-        words[replace_index] = synonym
+        replace_index = random.randint(0, len(words) - 2)
+        word = words[replace_index]
+        synonyms = wn.synsets(word)
+        if len(synonyms) > 0:
+            synonym = synonyms[0].lemmas()[0].name()
+            words[replace_index] = synonym
     return " ".join(words)
 
 
-test_sentence = """
-Which countries share a border with Morocco?
+def augment_sentence(sentence: str) -> str:
+    """
+        WIP:
 
-['sahara', 'western sahara', 'mauritania', 'algeria', 'spain']"""
+        Augments the input sentence with Hypernymy and Hyponymy information.
+        Might need some kind of NER to find nouns...
+    :param sentence:
+    :return:
+    """
+    words = sentence.split(" ")
+    for i in range(5):
+        replace_index = random.randint(0, len(words) - 2)
+        word = words[replace_index]
 
-print(distort_sentence(test_sentence))
+        for synset in wn.synsets(word):
+            for hyper in synset.hypernyms():
+                print(synset, hyper)
+                words[replace_index] = hyper
+    return " ".join(words)
+
+
+test_sentence = """Which countries share a border with Morocco?"""
+
+# Uncomment to test
+# print(distort_sentence(test_sentence))
+# print(augment_sentence(test_sentence))
